78eb3b320aa51a98e5d563c33b48747641b01ab9
Saurav <saurav.maharjan7@gmail.com>
2024-02-14 22:33:35 +0545

    feat : query for createUser, getUser, updateUser

--- services/user.service.impl.go
+++ services/user.service.impl.go
@@ -2,8 +2,10 @@ package services
 
 import (
 	"context"
+	"errors"
 	"saurav/mongo/models"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -20,11 +22,15 @@ func NewUserService(userCollection *mongo.Collection, ctx context.Context) UserS
 }
 
 func (u *UserServiceImpl) CreateUser(user *models.User) error {
-	return nil
+	_,err := u.userCollection.InsertOne(u.ctx, user)
+	return err
 }
 
 func (u *UserServiceImpl) GetUser(userName *string) (*models.User, error) {
-	return nil, nil
+	var user *models.User
+	query := bson.D{bson.E{Key:"user_name",Value:userName}}
+	err := u.userCollection.FindOne(u.ctx, query).Decode(&user)
+	return user, err
 }
 
 func (*UserServiceImpl) GetAll() ([]*models.User, error) {
@@ -32,6 +38,12 @@ func (*UserServiceImpl) GetAll() ([]*models.User, error) {
 }
 
 func (u *UserServiceImpl) UpdateUser(user *models.User) error {
+	filter := bson.D{bson.E{Key:"user_name",Value:user.Name}}
+	update := bson.D{bson.E{Key:"$set",Value: bson.D{bson.E{Key:"user_name",Value: user.Name},bson.E{Key:"user_age",Value: user.Age},bson.E{Key:"user_adddress",Value: user.Address}}}}
+	result,_ := u.userCollection.UpdateOne(u.ctx,filter,update)
+	if result.MatchedCount != 1 {
+		return errors.New("no matched document found for update")
+	}
 	return nil
 }
 
